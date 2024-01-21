38812d90cc4913d392fef0fe18504cf54f9b1c81
Aidennn-Lee <yw010804@gmail.com>
2024-01-21 18:00:41 +0900

    paging

--- src/main/java/com/sbb/sbb3/question/repository/QuestionRepository.java
+++ src/main/java/com/sbb/sbb3/question/repository/QuestionRepository.java
@@ -1,8 +1,15 @@
 package com.sbb.sbb3.question.repository;
 
 import com.sbb.sbb3.question.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
+import java.util.List;
 
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findByTitle(String title);
+    Question findByTitleAndContent(String title, String content);
+    List<Question> findByTitleLike(String title);
+    Page<Question> findAllBy(Pageable pageable);
 }
--- src/test/java/com/sbb/sbb3/Sbb3ApplicationTests.java
+++ src/test/java/com/sbb/sbb3/Sbb3ApplicationTests.java
@@ -2,6 +2,7 @@ package com.sbb.sbb3;
 
 import com.sbb.sbb3.answer.repository.AnswerRepository;
 import com.sbb.sbb3.question.repository.QuestionRepository;
+import com.sbb.sbb3.question.service.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,12 +14,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class Sbb3ApplicationTests {
 
     @Autowired
-    private QuestionRepository questionRepository;
-
-    @Autowired
-    private AnswerRepository answerRepository;
+    private QuestionService questionService;
 
     @Test
-    void testJpa() {
+    void testJpa(){
+        for(int i = 1; i <= 300; i++){
+            String title = String.format("테스트 데이터 [%03d]", i);
+            String content = "내용 없음";
+            this.questionService.create(title, content);
+
+        }
     }
 }
