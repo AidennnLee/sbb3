5435bd4d4cd7ee562e0bd1e3243c930c20585006
Aidennn-Lee <yw010804@gmail.com>
2024-01-22 10:35:10 +0900

    member email check

--- src/main/java/com/sbb/sbb3/member/controller/MemberController.java
+++ src/main/java/com/sbb/sbb3/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.sbb.sbb3.member.form.MemberCreateForm;
 import com.sbb.sbb3.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,7 +31,18 @@ public class MemberController {
             bindingResult.rejectValue("passwordConfirm", "passwordIncorrect", "패스워드가 일치하지 않습니다.");
             return "member/signup_form";
         }
-        memberService.create(memberCreateForm.getUsername(), memberCreateForm.getEmail(), memberCreateForm.getPassword());
+        try {
+            memberService.create(memberCreateForm.getUsername(),
+                    memberCreateForm.getEmail(), memberCreateForm.getPassword());
+        } catch (DataIntegrityViolationException e){
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "member/signup_form";
+        } catch (Exception e){
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "member/signup_form";
+        }
 
         return "redirect:/";
     }
