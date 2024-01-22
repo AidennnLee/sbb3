2fa8dd7dabee71742275bc3c69a7e74d9b51a329
Aidennn-Lee <yw010804@gmail.com>
2024-01-22 11:28:08 +0900

    login

---
+++ src/main/java/com/sbb/sbb3/member/MemberSecurityService.java
@@ -0,0 +1,40 @@
+package com.sbb.sbb3.member;
+
+import com.sbb.sbb3.member.entity.Member;
+import com.sbb.sbb3.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class MemberSecurityService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        Optional<Member> _member = this.memberRepository.findByUsername(username);
+        if(_member.isEmpty()){
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+        }
+        Member member = _member.get();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        if ("admin".equals(username)) {
+            authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue()));
+        } else {
+            authorities.add(new SimpleGrantedAuthority(MemberRole.USER.getValue()));
+        }
+        return new User(member.getUsername(), member.getPassword(), authorities);
+    }
+}
--- src/main/java/com/sbb/sbb3/member/repository/MemberRepository.java
+++ src/main/java/com/sbb/sbb3/member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.sbb.sbb3.member.repository;
 import com.sbb.sbb3.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
 }
